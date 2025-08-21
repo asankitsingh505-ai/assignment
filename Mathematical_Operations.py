0269e169b87d652fd7729d24665dbdba20434583
asankitsingh505-ai <asankitsingh505@gmail.com>
2025-08-21 21:31:21 +0530

    Create Mathematical_Operations.py

---
+++ Mathematical_Operations.py
@@ -0,0 +1,13 @@
+a = input("Enter the first number: ")
+b = input("Enter the second number:")
+a = int (a)
+b = int (b)
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+
+print("\naddition: ", c)
+print("subtraction: ", d)
+print("multiplication: ", e)
+print("division: ", f)
